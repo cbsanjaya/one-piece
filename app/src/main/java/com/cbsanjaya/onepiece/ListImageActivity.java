08193d55ced551cbcba2772302eebc4498ff26ae
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-08-01 15:18:00 +0700

    load image from url to imageview.

--- app/src/main/java/com/cbsanjaya/onepiece/ListImageActivity.java
+++ app/src/main/java/com/cbsanjaya/onepiece/ListImageActivity.java
@@ -1,6 +1,7 @@
 package com.cbsanjaya.onepiece;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,7 @@ import android.widget.ImageView;
 
 import com.cbsanjaya.onepiece.utils.NetUtils;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -106,23 +109,53 @@ public class ListImageActivity extends AppCompatActivity {
 
     class ImageViewHolder extends RecyclerView.ViewHolder {
 
+        private static final String TAG = "ImageViewHolder";
         ImageView imageView;
 
         public ImageViewHolder(View view) {
             super(view);
 
-            imageView = (ImageView) view.findViewById(R.id.imageView);
+            imageView = view.findViewById(R.id.imageView);
         }
 
         public void bindToHolder(Context context, String url) {
-            Picasso.with(context) //
-                    .load(url) //
-                    .placeholder(R.drawable.placeholder) //
-                    .error(R.drawable.error) //
-                    .fit() //
-                    .tag(context) //
+            Transformation transformation = new Transformation() {
+
+                @Override
+                public Bitmap transform(Bitmap source) {
+                    Bitmap result = source;
+
+                    int targetWidth = imageView.getWidth();
+
+                    if (targetWidth != 0) {
+                        Log.i(TAG, "sourceWidth: " + source.getWidth() + ", sourceHeight: " + source.getHeight());
+                        double aspectRatio = (double) source.getHeight() / (double) source.getWidth();
+                        int targetHeight = (int) (targetWidth * aspectRatio);
+                        Log.i(TAG, "targetWidth: " + targetWidth + ", targetHeight: " + targetHeight);
+                        result = Bitmap.createScaledBitmap(source, targetWidth, targetHeight, false);
+                        if (result != source) {
+                            // Same bitmap is returned if sizes are the same
+                            source.recycle();
+                        }
+                    }
+
+                    return result;
+                }
+
+                @Override
+                public String key() {
+                    return "transformation" + " desiredWidth";
+                }
+            };
+
+            Picasso.with(context)
+                    .load(url)
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.error)
+                    .transform(transformation)
                     .into(imageView);
         }
+
     }
 
     class DownloadTask extends AsyncTask<String, Void, List<String>> {
