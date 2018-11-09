b0a9a576c1186e2187c08edd0bdfb8e3bb778297
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-11-10 04:03:12 +0700

    download chapter

--- server/api/index.php
+++ server/api/index.php
@@ -1,9 +1,41 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
-});
+// auto load class
+// spl_autoload_register(function ($class_name) {
+//     include $class_name . '.php';
+// });
 
-$komik = new Models\Comic("one_piece", "1", array("lkj", "123"));
+// $komik = new Models\Comic("one_piece", "1", array("lkj", "123"));
 
-echo json_encode($komik);
+// echo json_encode($komik);
+
+include('./lib/download_chapter.php');
+
+if (!isset($_SERVER["PATH_INFO"])) {
+    die("Route Can't be Empty");
+}
+
+$pathInfo = $_SERVER["PATH_INFO"];
+
+$trim = trim($pathInfo, '/');
+
+$url = explode("/", $trim);
+
+if (count($url) == 1) {
+    $file = __DIR__ . '/comic/' . $url[0] . '/index.json';
+    if (!file_exists($file)) {
+        echo 'Comic ' . $url[0] . ' not Found';
+        exit();
+    }
+
+    readfile($file);
+} elseif (count($url) == 2) {
+    $file = __DIR__ . '/comic/' . $url[0] . '/' . $url[1] . '.json';
+    if (file_exists($file)) {
+        readfile($file);
+    } else {
+        comicChapter($file, $url[0], $url[1]);
+    }
+} else {
+    die("Route not Found");
+}
---
+++ server/api/lib/download_chapter.php
@@ -0,0 +1,44 @@
+<?php
+
+// include lib
+include('simple_html_dom.php');
+
+function comicChapter($file, $comic, $chapter)
+{
+    $baseDomain = "http://www.mangacanblog.com/";
+    $baseUrl = $baseDomain . "baca-komik-" . $comic . "-%s-%d-bahasa-indonesia-" . $comic . "-%s-terbaru.html";
+    $url = sprintf($baseUrl, $chapter, $chapter + 1, $chapter);
+
+    $html = file_get_html($url);
+    
+    $obj = new \stdClass;
+    $obj->comic = $comic;
+    $obj->chapter = $chapter;
+
+    $imageSource = array();
+    foreach ($html->find('div#manga img') as $e) {
+        $imageUrl = $e->src;
+        $firstUrl = substr($imageUrl, 0, 4);
+        if ($firstUrl != "http") {
+            $imageUrl = $baseDomain . $imageUrl;
+        }
+
+        $imageSource[] = $imageUrl;
+    }
+
+    $obj->images = $imageSource;
+    $allData = json_encode($obj, JSON_UNESCAPED_SLASHES);
+
+    if (count($imageSource) == 0) {
+        echo "Komik " . $comic . " Chapter " . $chapter . " Tidak ditemukan";
+        exit();
+    }
+    
+    $index = $allData;
+    echo $index;
+    
+    //save the file...
+    $fh = fopen($file, "w");
+    fwrite($fh, $index);
+    fclose($fh);
+}
